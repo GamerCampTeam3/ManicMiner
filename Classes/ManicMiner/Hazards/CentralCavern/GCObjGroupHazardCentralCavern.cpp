c34b256b70783a4ab956f14f4ad4c220fa1d7957
Dave O'Dwyer <dave.Odwyer@mail.bcu.ac.uk>
2020-10-20 12:19:28 +0100

    Updates to configure /Resources folder required for Hazard programmer art.

--- Classes/ManicMiner/Hazards/CentralCavern/GCObjGroupHazardCentralCavern.cpp
+++ Classes/ManicMiner/Hazards/CentralCavern/GCObjGroupHazardCentralCavern.cpp
@@ -10,7 +10,7 @@
 // Static Creation Params lifted out of GCObjHazard to here to allow GCObjHazard to be a class which can represent differnt Hazard sprites/animations/physics.
 
 static CGCFactoryCreationParams s_cCreationParams_CGCObj_EBush("CGCObjHazard_EBush", "TexturePacker/Sprites/Hazards/bush/bush.plist", "bush", b2_dynamicBody, true);
-static CGCFactoryCreationParams s_cCreationParams_CGCObj_EDownwardSpike("CGCObjHazard_EBush", "TexturePacker/Sprites/Hazards/downwardSpike/downwardSpike.plist", "downwardSpike", b2_dynamicBody, true);
+static CGCFactoryCreationParams s_cCreationParams_CGCObj_EDownwardSpike("CGCObjHazard_EDownwardSpike", "TexturePacker/Sprites/Hazards/downwardSpike/downwardSpike.plist", "downwardSpike", b2_dynamicBody, true);
 //////////////////////////////////////////////////////////////////////////
 //
 //////////////////////////////////////////////////////////////////////////
@@ -38,8 +38,8 @@ void CGCObjGroupHazardCentralCavern::VOnGroupResourceAcquire()
 	CGCObjHazard* pHazard3 = new CGCObjHazard(cocos2d::Vec2(1450.0f, 680.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EBush);
 	CGCObjHazard* pHazard4 = new CGCObjHazard(cocos2d::Vec2(1650.0f, 680.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EBush);
 
-	CGCObjHazard* pHazard5 = new CGCObjHazard(cocos2d::Vec2(740.0f, 1000.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EDownwardSpike);
-	CGCObjHazard* pHazard6 = new CGCObjHazard(cocos2d::Vec2(940.0f, 1000.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EDownwardSpike);
+	CGCObjHazard* pHazard5 = new CGCObjHazard(cocos2d::Vec2(740.0f, 1000.0f), HazardTypes::EHazardId::EDownwardSpike, s_cCreationParams_CGCObj_EDownwardSpike);
+	CGCObjHazard* pHazard6 = new CGCObjHazard(cocos2d::Vec2(940.0f, 1000.0f), HazardTypes::EHazardId::EDownwardSpike, s_cCreationParams_CGCObj_EDownwardSpike);
 
 }
 
