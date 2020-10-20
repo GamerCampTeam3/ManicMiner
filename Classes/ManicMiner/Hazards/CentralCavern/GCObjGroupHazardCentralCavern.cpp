78f2c1f2e24c9ce76cdfab6d26b0fcf72c78be9b
Dave O'Dwyer <dave.Odwyer@mail.bcu.ac.uk>
2020-10-20 09:20:15 +0100

    Updates to add programmer art to Hazards for Central Cavern level.
    - SourceAsset files updated to add 2 x programmer art for Hazards (bush, downwardSpike).
    - Mod to GCObjGroupHazardCentralCavern to add static creation params for new hazard art.

--- Classes/ManicMiner/Hazards/CentralCavern/GCObjGroupHazardCentralCavern.cpp
+++ Classes/ManicMiner/Hazards/CentralCavern/GCObjGroupHazardCentralCavern.cpp
@@ -9,7 +9,8 @@
 
 // Static Creation Params lifted out of GCObjHazard to here to allow GCObjHazard to be a class which can represent differnt Hazard sprites/animations/physics.
 //static CGCFactoryCreationParams s_cCreationParams_CGCObj_EBush("CGCObjHazard_EBush", "TexturePacker/Sprites/KoopaTrooper/KoopaTrooper.plist", "koopa", b2_staticBody, true);
-static CGCFactoryCreationParams s_cCreationParams_CGCObj_EBush("CGCObjHazard_EBush", "TexturePacker/Sprites/egg/egg.plist", "egg", b2_dynamicBody, true);
+static CGCFactoryCreationParams s_cCreationParams_CGCObj_EBush("CGCObjHazard_EBush", "TexturePacker/Sprites/Hazards/bush/bush.plist", "bush", b2_dynamicBody, true);
+static CGCFactoryCreationParams s_cCreationParams_CGCObj_EDownwardSpike("CGCObjHazard_EBush", "TexturePacker/Sprites/Hazards/downwardSpike/downwardSpike.plist", "downwardSpike", b2_dynamicBody, true);
 //////////////////////////////////////////////////////////////////////////
 //
 //////////////////////////////////////////////////////////////////////////
@@ -24,62 +25,10 @@ CGCObjGroupHazardCentralCavern::~CGCObjGroupHazardCentralCavern()
 
 //////////////////////////////////////////////////////////////////////////
 // virtual - overrides base class CGCObjGroupHazard
-// This function allocates an animation to an enemy.
-// First the enemy type is queried via the function GetHazardIdentifier() 
-// (this identifier was passed into the enemy when it was instantiated).
-// Secondly the HazardIdentifier is used to index a unordered_map of
-// Hazard Identifiers against the associated animation.
-// Note the commented out code below shows how the system would be expanded for 
-// a level with more than one enemey.
-void CGCObjGroupHazardCentralCavern::VOnGroupResourceAcquire_PostObject()
-{
-	// Call parent class
-	CGCObjGroupHazard::VOnGroupResourceAcquire_PostObject();
-	
-		// set up animations for pAnimation group 1
-	const char* pszPlist2 = "TexturePacker/Sprites/KoopaTrooper/KoopaTrooper.plist";
-	const char* pszAnim_Fly2 = "Fly";
-
-	// make an animation
-	cocos2d::ValueMap& rdictPList1 = GCCocosHelpers::CreateDictionaryFromPlist(pszPlist2);
-	cocos2d::Animation* pAnimation1 = GCCocosHelpers::CreateAnimation(rdictPList1, pszAnim_Fly2);
-	
-	// set up animations for pAnimation group 2 - NOTE EXAMPLE FOR FUTURE LEVELS.
-	//const char* pszPlist_Coin = "TexturePacker/Sprites/Coin/Coin.plist";
-	//const char* pszAnim_Coin_Rotate = "Rotate";
-
-	//cocos2d::ValueMap& rdicPList2 = GCCocosHelpers::CreateDictionaryFromPlist(pszPlist_Coin);
-	//cocos2d::Animation* pAnimation2 = GCCocosHelpers::CreateAnimation(rdicPList2, pszAnim_Coin_Rotate);
-
-	// Create a dictionary of HazardId's mapped to their corresponding animation.
-	std::unordered_map<HazardTypes::EHazardId, cocos2d::Animation*> cAnimationLookup = {
-
-		{HazardTypes::EHazardId::EBush, pAnimation1}
-		//{HazardTypes::EHazardId::EPenguin, pAnimation2},  <----Example for level with multiple enemies.
-
-	};
-
-	ForEachObject( [&] ( CGCObject* pObject) -> bool
-	{
-		CCAssert( ( GetGCTypeIDOf( CGCObjHazard ) == pObject->GetGCTypeID() ),
-			"CGCObject derived type mismatch!" );
-
-		CGCObjSprite* pItemSprite = (CGCObjSprite*)pObject;
-		CGCObjHazard* pItemObj = (CGCObjHazard*)pObject;
-
-		// Create the animation for each Hazard in the group according to the Hazard Identifier indexed into the cAnimationLookup map
-		//pItemSprite->RunAction(GCCocosHelpers::CreateAnimationActionLoop( cAnimationLookup[pItemObj->GetHazardIdentifier()]));
-
-		return true;
-	} );
-}
-
-//////////////////////////////////////////////////////////////////////////
-// virtual - overrides base class CGCObjGroupHazard
 // This function instantiates all required enemies for the Central Cavern level.
 //
 
-void CGCObjGroupHazardCentralCavern::VOnGroupResourceAcquire() 
+void CGCObjGroupHazardCentralCavern::VOnGroupResourceAcquire()
 {
 	// Call parent class
 	CGCObjGroupHazard::VOnGroupResourceAcquire();
@@ -89,10 +38,9 @@ void CGCObjGroupHazardCentralCavern::VOnGroupResourceAcquire()
 	CGCObjHazard* pHazard3 = new CGCObjHazard(cocos2d::Vec2(1450.0f, 680.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EBush);
 	CGCObjHazard* pHazard4 = new CGCObjHazard(cocos2d::Vec2(1650.0f, 680.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EBush);
 
-	CGCObjHazard* pHazard5 = new CGCObjHazard(cocos2d::Vec2(740.0f, 950.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EBush);
-	CGCObjHazard* pHazard6 = new CGCObjHazard(cocos2d::Vec2(940.0f, 950.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EBush);
+	CGCObjHazard* pHazard5 = new CGCObjHazard(cocos2d::Vec2(740.0f, 1000.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EDownwardSpike);
+	CGCObjHazard* pHazard6 = new CGCObjHazard(cocos2d::Vec2(940.0f, 1000.0f), HazardTypes::EHazardId::EBush, s_cCreationParams_CGCObj_EDownwardSpike);
 
-	//pHazard->SetName("H1");
 }
 
 
