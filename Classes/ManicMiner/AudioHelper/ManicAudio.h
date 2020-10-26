b698bf9b798f665c0563a05003632cdee57975d6
hehehenrique <hehehenrique@users.noreply.github.com>
2020-10-26 16:35:31 +0000

    Commented ManicAudio.h and CGameInstance files

--- Classes/ManicMiner/AudioHelper/ManicAudio.h
+++ Classes/ManicMiner/AudioHelper/ManicAudio.h
@@ -1,3 +1,13 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Gamer Camp 2020 / Henrique															//
+//////////////////////////////////////////////////////////////////////////////////////////
+// ManicAudio.h acts as a helper header file to play audio through						//
+// Cocos2d's SimpleAudioEngine:															//
+// This page contains the ESoundName enums and their respective							//
+// audio file directory pair, configured with an std::map < ESoundName, std::string >.	//
+// Just pass the right sound name to PlaySoundEffect() or PreloadSoundEffect()			//
+//////////////////////////////////////////////////////////////////////////////////////////
+
 #ifndef __MANIC_AUDIO_HELPER__
 #define __MANIC_AUDIO_HELPER__
 
@@ -17,7 +27,7 @@ enum class ESoundName
 };
 
 // Declare std::map that links every ESoundName to an audio file directory, type std::string
-const static std::map <ESoundName, std::string> k_mapSoundFiles =
+const static std::map < ESoundName, std::string > k_mapSoundFiles =
 {
 	{ ESoundName::ESN_Death,				"audio/Transition.wav"					},
 	{ ESoundName::ESN_MusicTheme,			"audio/YouWouldNotBelieveYourEyes.wav"	},
@@ -37,6 +47,7 @@ inline void PlaySoundEffect( ESoundName eSoundName )
 	audioEngine->playEffect( sAudioDirectory, false, 1.0f, 1.0f, 1.0f );
 }
 
+// Helper function that preloads a SoundEffect, taking in its respective ESoundName
 inline void PreloadSoundEffect( ESoundName eSoundName )
 {
 	auto audioEngine = SimpleAudioEngine::getInstance();
