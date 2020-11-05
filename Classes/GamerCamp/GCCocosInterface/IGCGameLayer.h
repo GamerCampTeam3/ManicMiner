042e4b638c58e0a8782da2029c8f5330172618b8
hehehenrique <hehehenrique@users.noreply.github.com>
2020-11-05 17:57:25 +0000

    Fixed crash when loading first level in
    Had to recover a change I had done in IGCGameLayer.h for module 1, also turned game back to windowed to prevent crashes freezing my laptop.
    
    Game is crashing when a CManicLayer gets destroyed.

--- Classes/GamerCamp/GCCocosInterface/IGCGameLayer.h
+++ Classes/GamerCamp/GCCocosInterface/IGCGameLayer.h
@@ -205,7 +205,7 @@ public:
 
 	public:
 		// scale factor from meters to pixels - box2d units are all meters
-		#define IGCGAMELAYER_B2D_PIXELS_PER_METER	(20.0f)
+		#define IGCGAMELAYER_B2D_PIXELS_PER_METER	( 60.0f )
 
 		inline b2World*	B2dGetWorld				();
 		inline f32		B2dGetTimestep			();
@@ -306,7 +306,8 @@ public:
 		}
 
 		// add IGCGameLayer to the scene
-		pReturnScene->addChild( pIGCLayer, -1 );
+		// Henrique Edit -> set this child to have tag = 0
+		pReturnScene->addChild( pIGCLayer, -1, 0 );
 
 		return pReturnScene;
 	}
