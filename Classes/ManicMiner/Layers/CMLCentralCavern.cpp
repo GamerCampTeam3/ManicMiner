d5c034ef635eff6dbcc3a11d3ba9c736a07f2d69
Bibool <52705025+Bibool@users.noreply.github.com>
2020-11-23 17:55:26 +0100

    Revert to Level requirements

--- Classes/ManicMiner/Layers/CMLCentralCavern.cpp
+++ Classes/ManicMiner/Layers/CMLCentralCavern.cpp
@@ -31,7 +31,7 @@ void CMLCentralCavern::VOnCreate( void )
 	// szLevelPath:			Sets the path where the level will be found.
 	// It is important you initialize the values BEFORE CManicLayer::VOnCreate() is called -
 	// Otherwise, it will use a bunch of default data (I have added checks for that) and nothing will load.	
-	m_sLevelCreationParameters.sLevelValues			= SLevelValues( ECollectibleRequirements::Collectible, 1);
+	m_sLevelCreationParameters.sLevelValues			= SLevelValues( ECollectibleRequirements::Collectible, 5);
 	m_sLevelCreationParameters.pszLevelBackground	= static_cast<char*>("TexturePacker/Backgrounds/Placeholder/CN_Background.plist");
 	m_sLevelCreationParameters.szLevelPath			= "OgmoEditor/0_CentralCavern.oel";
 	m_sLevelCreationParameters.szLevelName			= "Central Cavern";
